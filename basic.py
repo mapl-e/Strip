965503f1d152b3f2d4b2da9ec46ccf4c2ab1cc21
Maple <70176410+mapl-e@users.noreply.github.com>
2023-04-15 19:13:30 -0500

    Made it more compact, organized it, Fixed some bugs.
    Patch 1, The thing is now working. I made the code more organized, removed some comments and yup thats basically it.

--- basic.py
+++ basic.py
@@ -1,139 +1,157 @@
-## Constants
-
+# These are the constants used in the code
 DIGITS = '0123456789'
 
-#Errors
-
+# This class defines the errors that can occur
 class Error:
-    def __init__(self, error_name, details):
+    # Initializes the error object with its properties
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
-        
-        def as_string(self):
-            result = f'{self.error_name}: {self.details}'
-            return result
-        
-class IllegalChar(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
+    
+    # Formats the error message as string
+    def as_string(self):
+        result  = f'{self.error_name}: {self.details}\n'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+        return result
 
-## Tokens
+# This class defines the illegal character errors
+class IllegalCharError(Error):
+    # Initializes the illegal character error object with its properties
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
-TT_INT = 'TT_INT'
-TT_FLOAT = 'FLOAT'
-TT_PLUS = 'PLUS'
-TT_MINUS = 'MINUS'
-TT_MUL = 'MUL'
-TT_DIV = 'DIV'
-TT_LPAREN = 'LPAREN'
-TT_RPAREN = 'RPAREN'
+# This class defines the position of the character in the code
+class Position:
+    # Initializes the position object with its properties
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
 
+    # Advances the position when a new character is encountered
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+
+        return self
+
+    # Creates a copy of the position object
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
+# These are the token types used in the code
+TT_INT		= 'INT'
+TT_FLOAT    = 'FLOAT'
+TT_PLUS     = 'PLUS'
+TT_MINUS    = 'MINUS'
+TT_MUL      = 'MUL'
+TT_DIV      = 'DIV'
+TT_LPAREN   = 'LPAREN'
+TT_RPAREN   = 'RPAREN'
+
+# This class defines the token object
 class Token:
+    # Initializes the token object with its properties
     def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
-
+    
+    # Returns the string representation of the token object
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
-    
 
-
-# Lexer
-
+# This class defines the lexer object
 class Lexer:
-    def __init__(self, text):
+    # Initializes the lexer object with its properties
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
+    
+    # Advances the position and sets the current character accordingly
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+
+
+class Lexer:
+    def __init__(self, fn, text):
+        self.fn = fn    # filename
+        self.text = text    # input text
+        self.pos = Position(-1, 0, -1, fn, text)    # current position
+        self.current_char = None    # current character
+        self.advance()    # move to the first character
 
     def advance(self):
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     def make_tokens(self):
         tokens = []
 
-        # Addition
         while self.current_char != None:
             if self.current_char in ' \t':
                 self.advance()
             elif self.current_char in DIGITS:
-                tokens.append(self.make_numb())
+                tokens.append(self.make_number())
             elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
-                self.advance()
-
-        # Subtraction
-        while self.current_char != None:
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == '-':
                 tokens.append(Token(TT_MINUS))
                 self.advance()
-
-        # Multiplication
-        while self.current_char != None:
-            if self.current_char in ' \t':
-                self.advance()
             elif self.current_char == '*':
                 tokens.append(Token(TT_MUL))
-                self.advance()
-
-        # Division
-        while self.current_char != None:
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == '/':
                 tokens.append(Token(TT_DIV))
                 self.advance()
-
-        # Left Parenthesis
-        while self.current_char != None:
-            if self.current_char in ' \t':
-                self.advance()
             elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN))
-                self.advance()
-
-        # Right Parenthesis
-        while self.current_char != None:
-            if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
-
-        # Return error.
-        else:
-            char = self.current_char
-            self.advance()
-            return [], IllegalChar("'" + char + "'")
+            else:
+                # if the character is not recognized, raise an error
+                pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
 
         return tokens, None
 
-    def make_numb(self):
+    def make_number(self):
         num_str = ''
         dot_count = 0
 
         while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
-                if dot_count == 1:
-                    break
+                if dot_count == 1: break
                 dot_count += 1
                 num_str += '.'
             else:
                 num_str += self.current_char
-
             self.advance()
 
         if dot_count == 0:
             return Token(TT_INT, int(num_str))
         else:
             return Token(TT_FLOAT, float(num_str))
-
-def run(text):
-    lexer = Lexer(text)
+        
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+
     return tokens, error
